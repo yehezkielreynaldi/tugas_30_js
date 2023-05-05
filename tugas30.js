6f1be7542d4cab7700b22af116d9d3d173b07bc3
Yehezkiel Reynaldi Abraham <yehezkielreynaldi01@gmail.com>
2023-05-05 22:47:37 +0700

    Tugas 30 JS
    Tugas ini saya kirimkan untuk Niomic

---
+++ tugas30.js
@@ -0,0 +1,6 @@
+function panggilRegexp(value) {
+    let ambilData = value.match(/[es]/g);
+    console.log(ambilData);
+}
+
+panggilRegexp("Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat");
